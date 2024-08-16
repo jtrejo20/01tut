dea1b02a448c606d22c6d29bab5d67acf253d571
jtrejo-19 <128644977+jtrejo-19@users.noreply.github.com>
2024-08-15 18:29:46 -0700

    Initialized Footer.js, Header.js and Content.js; Updated index.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer.js';
 
 function App() {
   
 
   return (
     <div className="App">
-      <Header/>
-      
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
