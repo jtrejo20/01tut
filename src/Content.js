758c7e220582904b51ed53500ce5752db5b03118
jtrejo-19 <128644977+jtrejo-19@users.noreply.github.com>
2024-08-17 13:46:42 -0700

    Modified Content.js; Added basic useState hook function, changes name when Change Name button onClick event occurs

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
+
 const Content = () => {
+    const [name, setName] = useState('Dave');
+    const [count, setCount] = useState(0);
+
     const handleNameChange = () =>{
         const names = ['Bob', 'Kevin', 'Dave'];
         const int = Math.floor(Math.random()*3);
-        return names[int];
+        setName(names[int]);
       }
+
+    const handleClick = () => {
+      setCount(count + 1)
+      console.log(count)
+    } 
+
+    const handleClick2 = () => {
+      console.log(count)
+    }
+
   return (
     <main>
-        <p>
-          Hello {handleNameChange()}!
+        <p onDoubleClick={handleClick}>
+          Hello {name}!
         </p>
+        <button onClick={handleNameChange}>Change Name</button>
+        <button onClick={handleClick}>Click It</button>
+        <button onClick={handleClick2}>Click It</button>
     </main>
   )
 }
